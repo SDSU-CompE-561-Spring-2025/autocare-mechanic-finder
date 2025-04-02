b41bc9ac25e76a41d9413353c84373a19c7890da
GioLopez23 <giolopezunam@gmail.com>
2025-04-01 22:30:14 -0700

    Car Routes
    Roughly added car routes. Changes in main to include car routes .py file

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 
 from app.core.database import Base, engine
 from app.routes.user import router as user_router
+from app.routes.car import router as car_router
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(user_router, prefix="/users", tags=["users"])
+app.include_router(car_router, prefix="/cars", tags=["cars"])
 
 @app.get("/")
 def read_root():
