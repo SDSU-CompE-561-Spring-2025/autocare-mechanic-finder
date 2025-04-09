24679a5b50ff6bb44b22b3acf97b23288d9c93a2
ZAAY96 <zaiverayarza@gmail.com>
2025-04-08 18:13:20 -0700

    Updated users entering required fields

--- backend/src/app/schemas/users.py
+++ backend/src/app/schemas/users.py
@@ -1,2 +1,32 @@
 from datetime import datetime
 from pydantic import BaseModel, constr, EmailStr, Field
+
+#referencing professors backend for user
+
+
+class UserBase(BaseModel):
+    username: constr(min_length= 3, max_length= 32, pattern=r"^[a-zA-Z0-9_]+$")
+    email: EmailStr
+
+class UserCreate(UserBase):
+    password: constr(min_length= 3, max_length= 32, pattern=r"^[a-zA-Z0-9_]+$")
+
+class User(UserBase):
+    id: int = Field(..., gt=0)
+    username: constr(min_length= 3, max_length= 32, pattern=r"^[a-zA-Z0-9_]+$")
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+
+
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+    created_at: datetime
+
+
+    class Config:
+        from_attributes = True
