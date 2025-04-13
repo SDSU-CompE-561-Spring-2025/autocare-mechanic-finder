2f3e9105271bc8c22aac191046ff5f246fbf18ab
John C <59818168+johnchoi100403@users.noreply.github.com>
2025-04-13 16:49:10 -0700

    Update core

---
+++ backend/src/app/core/crud.py
@@ -0,0 +1,51 @@
+from sqlalchemy.orm import Session
+from sqlalchemy import func
+from . import schemas, models
+from passlib.context import CryptContext
+
+pwd = CryptContext(schemas=["bcrypt"], deprecated="auto")
+
+def get_pw_hash(password: str):
+    return pwd.hash(password)
+
+def verify_pw(defPwd:str, hashedPwd:str):
+    return pwd.verify(defPwd, hashedPwd)
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+def create_user(db: Session, user: schemas.UserCreate):
+    hashed_password = get_password_hash(user.password)
+    db_user = models.User(email=user.email, hashed_password=hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+#crud stuff
+
+# need to adjust based on database models format
+def get_service(db: Session, service_id: int):
+    return db.query(models.Service).filter(models.Service.id == service_id).first()
+
+def get_services(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Service).offset(skip).limit(limit).all()
+
+def create_service(db: Session, service: schemas.ServiceCreate):
+    db_service = models.Service(**service.dict())
+    db.add(db_service)
+    db.commit()
+    db.refresh(db_service)
+    return db_service
+
+
+
+
+
+
