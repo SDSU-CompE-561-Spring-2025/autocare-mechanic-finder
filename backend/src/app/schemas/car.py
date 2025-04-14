505c2f8d528b8b42b16763314a0e73d8015cbbab
ZAAY96 <zaiverayarza@gmail.com>
2025-04-13 17:39:23 -0700

    Changed main/car/database

--- backend/src/app/schemas/car.py
+++ backend/src/app/schemas/car.py
@@ -3,15 +3,26 @@ from enum import Enum
 
 class CarType(str, Enum):
     car = "car"
+    year = 2024
+    mileage = 300000
 
 
 class CarBase(BaseModel):
     car_name: str
-    car_type: CarType
+    Brand_type: CarType
+    mile_age: int
+    year: int
+    Trim: str
+    Last_Oil_Change: str
+    AirFilter: str
+
 
 
 class CarCreate(CarBase):
     pass
 
-class CarResponse():
-    #needs to be in
+class CategoryResponse(CarBase):
+    id: int
+
+    class Config:
+        from_attributes = True
