f11c0af9322a3b1cf2e268341d1ebcd3d5c9fc82
John C <59818168+johnchoi100403@users.noreply.github.com>
2025-04-13 17:04:45 -0700

    updated crud

--- backend/src/app/core/crud.py
+++ backend/src/app/core/crud.py
@@ -5,20 +5,26 @@ from passlib.context import CryptContext
 
 pwd = CryptContext(schemas=["bcrypt"], deprecated="auto")
 
+
 def get_pw_hash(password: str):
     return pwd.hash(password)
 
-def verify_pw(defPwd:str, hashedPwd:str):
+
+def verify_pw(defPwd: str, hashedPwd: str):
     return pwd.verify(defPwd, hashedPwd)
+
 
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
 
+
 def get_user_by_email(db: Session, email: str):
     return db.query(models.User).filter(models.User.email == email).first()
 
+
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
+
 
 def create_user(db: Session, user: schemas.UserCreate):
     hashed_password = get_password_hash(user.password)
@@ -28,14 +34,18 @@ def create_user(db: Session, user: schemas.UserCreate):
     db.refresh(db_user)
     return db_user
 
-#crud stuff
+# crud stuff
 
 # need to adjust based on database models format
+
+
 def get_service(db: Session, service_id: int):
     return db.query(models.Service).filter(models.Service.id == service_id).first()
 
+
 def get_services(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Service).offset(skip).limit(limit).all()
+
 
 def create_service(db: Session, service: schemas.ServiceCreate):
     db_service = models.Service(**service.dict())
@@ -43,9 +53,3 @@ def create_service(db: Session, service: schemas.ServiceCreate):
     db.commit()
     db.refresh(db_service)
     return db_service
-
-
-
-
-
-
