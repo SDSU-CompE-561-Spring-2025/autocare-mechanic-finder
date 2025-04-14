b57f1ee3073cde36d61e3fc00ec5bd3c05d7dc77
Nick Woodson <nwoodson3099@gmail.com>
2025-04-13 17:10:21 -0700

    Create models.py

---
+++ backend/src/app/models/models.py
@@ -0,0 +1,40 @@
+#models
+
+from sqlalchemy import Column, Integer, String, DateTime
+from database import Base
+
+#Inspired by Ugar Dogan's model.py example
+class User(Base):
+    __tablename__ = 'users'
+
+    user_id = Column(Integer, primary_key=True, AUTO_INCREMENT=True, nullable=False, index=True)
+    name = Column(String, required=True, nullable=False)
+    email = Column(String, required=True, unique=True, nullable=False)
+    password = Column(String, required=True, nullable=False)
+    State = Column(String)
+    Cars = Column(String)
+    created_at = Column(DateTime, default=DateTime.utcnow)
+
+class Car(Base):
+    __tablename__ = 'cars'
+
+    Car_id = Column(Integer, primary_key=True, index=True)
+    Cars = Column(String, foreign_key=True, AUTO_INCREMENT=True)
+    BrandName = Column(String)
+    Model = Column(String)
+    Year = Column(Integer)
+    Mileage = Column(Integer)
+    Trim = Column(String)
+    LastOilChange = Column(String)
+    AirFilter = Column(String)
+    created_at = Column(DateTime, defualt=DateTime.utcnow)
+
+class Login(Base):
+
+    Token_Id = Column(String, primary_key=True, AUTO_INCREMENT=True)
+    user_id = Column(Integer,foreign_key=True,nullable=True)
+    Auth = Column(bool, required=True)
+    Error_type = Column(String)
+    Error_message = Column(String)
+    created_at = Column(DateTime, default=DateTime.utcnow)
+ 
