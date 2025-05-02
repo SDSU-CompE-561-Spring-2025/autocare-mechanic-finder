245e034a2f48587cb10d948291993ca0ed8ea4f9
GioLopez23 <giolopezunam@gmail.com>
2025-05-02 16:42:15 -0700

    Added husky, prettier, prettier config and update info page files

---
+++ frontend/src/app/(updateinfo)/carupdate/page.tsx
@@ -0,0 +1,5 @@
+export default function UserUpdatePage() {
+    return(<div className="flex flex-col justify-center items-center ">
+        <h1>CarUpdatePage</h1>
+        </div>)
+}
---
+++ frontend/src/app/(updateinfo)/userupdate/page.tsx
@@ -0,0 +1,5 @@
+export default function UserUpdatePage() {
+    return(<div className="flex flex-col justify-center items-center ">
+        <h1>UserUpdatePage</h1>
+        </div>)
+}
