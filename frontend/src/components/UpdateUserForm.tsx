9f7610d815a3ac20da2572480c10923becae5cd1
GioLopez23 <giolopezunam@gmail.com>
2025-05-02 18:48:55 -0700

    Fixed text color on update user form, was changed with ShadCN installation

--- frontend/src/components/UpdateUserForm.tsx
+++ frontend/src/components/UpdateUserForm.tsx
@@ -2,31 +2,31 @@ export default function UpdateUserForm() {
     return(
         <div className="flex flex-col self-center justify-center items-center bg-zinc-950 h-screen w-screen">
         <div className="flex flex-col justify-center items-center p-5 bg-zinc-800 max-w-xl margin-auto rounded-xl">
-        <h1 className="mb-5 text-2xl font-bold">Update User</h1>
+        <h1 className="mb-5 text-2xl font-bold text-white">Update User</h1>
         <form>
             <div className="mb-5">
-                <label htmlFor="state">US State: (Optional)</label>
+                <label htmlFor="state" className="text-white">US State: (Optional)</label>
                 <input  type="text"
                         id="state"
                         name="state"
-                        className="bg-slate-200 rounded-md w-full text-black"/>
+                        className="bg-slate-300 rounded-md w-full text-black"/>
             </div>
             <div className="mb-5 items-center flex flex-row">
-                <label htmlFor="update_password">Update Password? (Optional)</label>
+                <label htmlFor="update_password" className="text-white">Update Password? (Optional)</label>
                 <input  type="checkbox"
                         id="update_password"
                         name="update_password"
                         className="h-4 w-4 ml-3 cursor-pointer accent-red-500"/>
             </div>
             <div className="mb-5">
-                <label htmlFor="new_password">New Password: </label>
+                <label htmlFor="new_password" className="text-white">New Password: </label>
                 <input  type="password"
                         id="new_password"
                         name="new_password"
                         className="bg-slate-200 rounded-md w-full text-black"/>
             </div>
             <div className="mb-5">
-                <label htmlFor="current_password">Current Password: </label>
+                <label htmlFor="current_password" className="text-white">Current Password: </label>
                 <input  type="password"
                         id="current_password"
                         name="current_password"
@@ -34,10 +34,10 @@ export default function UpdateUserForm() {
             </div>
             <div className="text-center w-full">
                 <button type="submit"
-                        className="py-3.5 px-10 bg-red-500 rounded-xl cursor-pointer text-xl font-bold">
+                        className="py-3.5 px-10 bg-red-500 rounded-xl cursor-pointer text-xl font-bold text-white">
                         Update</button>
                 <button type="submit"
-                        className="py-3.5 px-10 ml-6 bg-slate-500 rounded-xl cursor-pointer text-xl font-bold">
+                        className="py-3.5 px-10 ml-6 bg-slate-500 rounded-xl cursor-pointer text-xl font-bold text-white">
                         Cancel</button>
             </div>
         </form>
