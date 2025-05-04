d6f944975f478dc5809dbc8d62c14e692b54774c
GioLopez23 <giolopezunam@gmail.com>
2025-05-03 19:02:05 -0700

    Added CORSMiddleware on backend to enable connection between backend and frontend

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.database import Base, engine
 from app.routes.car import router as car_router
@@ -6,6 +7,14 @@ from app.routes.user import router as user_router
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user_router, prefix="/users", tags=["users"])
 app.include_router(car_router, prefix="/cars", tags=["cars"])
