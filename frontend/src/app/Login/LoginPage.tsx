61a2063e59badfc8294d5de5c625f020978a62b9
Nick Woodson <nwoodson3099@gmail.com>
2025-05-07 22:34:32 -0700

    Add files via upload

---
+++ frontend/src/app/Login/LoginPage.tsx
@@ -0,0 +1,8 @@
+'use cilent';
+
+// inspired ugur Dogan's lectures and examples
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import logo from '/frontend/public/images/MyCarCare_Full.png';
+import 
