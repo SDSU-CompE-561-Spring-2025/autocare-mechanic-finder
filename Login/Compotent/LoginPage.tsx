8c94deca9a80da975ccc923df5eeb9d0c9102b54
Nick Woodson <nwoodson3099@gmail.com>
2025-05-07 21:20:50 -0700

    Delete Login/Compotent directory

--- Login/Compotent/LoginPage.tsx
+++
@@ -1,8 +0,0 @@
-'use cilent';
-
-// inspired ugur Dogan's lectures and examples
-import { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import * as z from 'zod';
-import logo from '/frontend/public/images/MyCarCare_Full.png';
-import 
