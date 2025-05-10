9c69be64ee3fe58c29e3b099950804a8fdaca1c9
ZAAY96 <zaiverayarza@gmail.com>
2025-05-10 15:17:46 -0700

    Update SideBar.tsx
    Fixed Autoshop Finder Link, Small capitalization error

--- frontend/src/app/components/ui/SideBar.tsx
+++ frontend/src/app/components/ui/SideBar.tsx
@@ -19,7 +19,7 @@ export default function Sidebar({ menuOpen, toggleMenu }) {
         <a href="/dashboard" className="font-bold hover:underline">Home</a>
         <a href="/RegisterCarPage" className="font-bold hover:underline">Register New Car</a>
         <a href="/car-parts" className="font-bold hover:underline">Car Parts</a>
-        <a href="/AutoShopFinderPage" className="font-bold hover:underline">AutoShop Parts Finder</a>
+        <a href="/AutoshopFinderPage" className="font-bold hover:underline">AutoShop Finder</a>
         <a href="/update-account" className="font-bold hover:underline">Update Account</a>
       </nav>
 
