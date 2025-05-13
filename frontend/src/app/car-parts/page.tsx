d7036b4acb370239c1086ede02a719860e4606ba
GioLopez23 <giolopezunam@gmail.com>
2025-05-12 20:44:41 -0700

    Adjusted auto part link styling

--- frontend/src/app/car-parts/page.tsx
+++ frontend/src/app/car-parts/page.tsx
@@ -11,12 +11,12 @@ export default function CarPartsPage() {
             </div>
             <div className="p-2 bg-[#C4C4C4] rounded-xl w-full h-fit">
                 <div className="bg-white rounded-xl py-4 flex flex-col items-center justify-center">
-                  <a href="https://shop.advanceautoparts.com/" className='my-3'>Advanced Auto Parts</a>
-                  <a href="https://www.autozone.com/" className='mb-3'>Autozone</a>
-                  <a href="https://www.carparts.com/" className='mb-3'>CarParts.com</a>
-                  <a href="https://www.napaonline.com/" className='mb-3'>NAPA</a>
-                  <a href="https://www.oreillyauto.com/" className='mb-3'>O'Reilly Auto Parts</a>
-                  <a href="https://www.rockauto.com/" className='mb-3'>RockAuto</a>
+                  <a href="https://shop.advanceautoparts.com/" className='my-3 underline text-blue-500'>Advanced Auto Parts</a>
+                  <a href="https://www.autozone.com/" className='mb-3 underline text-blue-500'>Autozone</a>
+                  <a href="https://www.carparts.com/" className='mb-3 underline text-blue-500'>CarParts.com</a>
+                  <a href="https://www.napaonline.com/" className='mb-3 underline text-blue-500'>NAPA</a>
+                  <a href="https://www.oreillyauto.com/" className='mb-3 underline text-blue-500'>O'Reilly Auto Parts</a>
+                  <a href="https://www.rockauto.com/" className='mb-3 underline text-blue-500'>RockAuto</a>
                 </div>
             </div>
       </div>
